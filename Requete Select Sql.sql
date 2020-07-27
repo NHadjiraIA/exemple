50305825c6ab7e4c8f6afd351893e79d4a12a9bd
Nadjib Benkhellat <nadjib.benkhellat@relogix.com>
2020-07-27 02:38:01 -0400

    envois de mon fichier

---
+++ Requete Select Sql.sql
@@ -0,0 +1,16 @@
+--use database
+use Ventes ;
+Go
+
+--list of customers in their name begins with M
+Select * from client  C Where C.ContactNom like 'M%' ;
+Go
+
+--customer list with uppercase contact title
+ select  NomSociete, ContactNom, ContactTitre, upper(ContactTitre) as Titre, Adresse, Ville from client;
+ Go
+ --customer list from Canada
+  Select * from client C Where C.Pays = 'Canada';
+Go
+
+   
